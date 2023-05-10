538f6d7fb0fbc47992b34f2190881ded2bff70fb
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-05-10 21:32:11 +0300

    update validation

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,9 +2,16 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
+    const keys = Object.keys(req.body);
+    if (keys.length === 0) {
+      next(HttpError(400, "missing fields"));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      next(
+        HttpError(400, `missing required ${error.details[0].path[0]} field`)
+      );
     }
     next();
   };
--- models/contacts.js
+++ models/contacts.js
@@ -17,17 +17,6 @@ const getContactById = async (contactId) => {
   return result || null;
 };
 
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const result = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
-};
-
 const addContact = async (body) => {
   const contacts = await listContacts();
   const newContact = {
@@ -38,6 +27,17 @@ const addContact = async (body) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
   return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const result = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 };
 
 const updateContact = async (contactId, body) => {
