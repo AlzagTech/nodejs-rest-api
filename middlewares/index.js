1538960d5ac41dd696259c6d00a86fe45259079e
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-05-17 20:04:56 +0300

    update middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,5 @@
 const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
+const validatePatchBody = require("./validatePatchBody");
 
-module.exports = { validateBody };
+module.exports = { validateBody, isValidId, validatePatchBody };
