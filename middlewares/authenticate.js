f4ec367d34353ca5dc2bff1047e61180e68d9dc6
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-06-11 18:04:17 +0300

    create upload middlewares

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -18,7 +18,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user || !user.token || user.token !== token) {
+    if (!user.token) {
       next(HttpError(401, "Not authorized"));
     }
 
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,12 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const validatePatchBody = require("./validatePatchBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = { validateBody, isValidId, validatePatchBody, authenticate };
+module.exports = {
+  validateBody,
+  isValidId,
+  validatePatchBody,
+  authenticate,
+  upload,
+};
