7d1df3cb29b4796d1de27737213a42e07eb2df47
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-06-14 17:22:04 +0300

    add verification ctrl

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+
+const { BASE_URL } = process.env;
 
 const { User } = require("../../models/user");
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,12 +17,22 @@ const register = async (req, res) => {
 
   const hashPasword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPasword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
